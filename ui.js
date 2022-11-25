fee5bf64f1b835cd3e4352c34d7a3bb2f8f7c208
Frank141 <https://github.com/lanlan0214/TodayDo.git>
2022-11-25 15:12:21 +0800

    'Add repos' , 整合好的 ! 牛逼 好用

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
     this.profile = document.getElementById('profile');
   }
 
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
       <div class = "card card-body mb-3">
@@ -22,11 +23,75 @@ class UI {
             <li class="list-group-item">Website/Blog: ${user.blog}</li>
             <li class="list-group-item">Location: ${user.location}</li>
             <li class="list-group-item">Member Since: ${user.created_at}</li>
-          </ul>
+           </ul>
+          </div>
         </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div> 
     `;
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 1sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 1000);
+  }
+
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function (repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+          <span class ="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+          <span class ="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+          <span class ="badge badge-success">Forks: ${repo.form_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
   }
 }
